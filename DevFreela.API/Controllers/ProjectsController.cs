32899c27a22d7b5709df068e91f2fc8c039fa101
Ygor Lacerda <ygor.l@f360.com.br>
2021-06-12 09:24:49 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
